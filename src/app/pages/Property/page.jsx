4250353a96fe210d8af08ad914cb8f8790d8e308
Fasih Haider <136050495+half-developer@users.noreply.github.com>
2023-09-06 11:07:17 +0500

    Update page.jsx

--- src/app/pages/Property/page.jsx
+++ src/app/pages/Property/page.jsx
@@ -1,4 +1,4 @@
-import { Nav1page2 } from '@/app/SeacondComponents/NavBar1Page2/page';
+import Nav1page2  from '@/app/SeacondComponents/NavBar1Page2/Page1';
 import Nav2Page2 from '@/app/SeacondComponents/Navbar2Page2/page';
 import AllHouses from '@/app/SeacondComponents/Renthouses/AllHouses.jsx';
 import Footer from '@/app/SeacondComponents/SeacondFooter/Footer.jsx';
@@ -7,7 +7,6 @@ import React from 'react'
 const Properties=() => {
   return (
     <>
- 
   <Nav1page2 />
   <Nav2Page2/>
    <AllHouses/>
